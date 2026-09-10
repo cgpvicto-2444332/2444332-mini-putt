89df839152b639509a9c955c7a45eaffe11c813a
Didier Mathieu <dmathieu2007@gmail.com>
2026-09-10 09:47:12 -0400

    ajout d'une seconde caméra

---
+++ Assets/Scripts/GestionCamera.cs
@@ -0,0 +1,52 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class GestionnaireCameraJeu : MonoBehaviour
+{
+    [Header("Caméras")]
+    [SerializeField, Tooltip("Caméra libre contrôlée par le joueur (celle pilotée par CibleCamera)")]
+    private CinemachineCamera cameraLibre;
+
+    [SerializeField, Tooltip("Caméra fixe qui suit la balle une fois placée")]
+    private CinemachineCamera cameraSuiviBalle;
+
+    [SerializeField, Tooltip("Script qui gère les déplacements de la caméra libre (sur le même objet que cameraLibre)")]
+    private CibleCamera controleCameraLibre;
+
+    [Header("Priorités")]
+    [SerializeField, Tooltip("Priorité de la caméra active")]
+    private int prioriteActive = 20;
+
+    [SerializeField, Tooltip("Priorité de la caméra inactive")]
+    private int prioriteInactive = 0;
+
+    private void Start()
+    {
+        ActiverCameraLibre();
+    }
+
+    public void ActiverCameraLibre()
+    {
+        cameraLibre.Priority = prioriteActive;
+        cameraSuiviBalle.Priority = prioriteInactive;
+
+        if (controleCameraLibre != null)
+        {
+            controleCameraLibre.enabled = true;
+        }
+    }
+
+    public void ActiverCameraSuiviBalle(Transform balle)
+    {
+        cameraSuiviBalle.Follow = balle;
+        cameraSuiviBalle.LookAt = balle;
+
+        cameraSuiviBalle.Priority = prioriteActive;
+        cameraLibre.Priority = prioriteInactive;
+
+        if (controleCameraLibre != null)
+        {
+            controleCameraLibre.enabled = false;
+        }
+    }
+}
